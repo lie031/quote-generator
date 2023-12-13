26f19f0725635afac24c5791b11784da34e0d389
lie03 <139715955+lie031@users.noreply.github.com>
2023-12-13 18:32:12 -0500

    add share btn

--- main.js
+++ main.js
@@ -6,10 +6,21 @@ const newQuote = document.querySelector('.new-quote');
 
 
 newQuote.addEventListener('click',generateQuote);
+twtBtn.addEventListener('click', tweetQuote);
 
 function generateQuote(){
     let ramdomNum = Math.floor(Math.random()*quotes.length)
     quote.textContent = "“" + quotes[ramdomNum].quote + "”";
     autor.textContent = quotes[ramdomNum].author;
+}
+
+function tweetQuote() {
+    const currentQuote = quote.textContent;
+    const currentAuthor = autor.textContent;
+    
+    const twitterIntentURL = `https://twitter.com/intent/tweet?text=${encodeURIComponent(currentQuote)} - ${encodeURIComponent(currentAuthor)}`;
+
+    // Open the Twitter intent URL in a new window or tab
+    window.open(twitterIntentURL, '_blank');
 }
 
